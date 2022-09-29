aa0cf4984ca59c5e67f8e2bb88b86dde8eaab3f3
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-29 23:02:55 +0530

    Add: Welcome to Employee Wage Computation Program

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,16 @@
+package EmployeeWageComputation;
+
+public class EmployeeWageComputation {
+
+	// Start with displaying welcome message
+	public void printMessage() {
+		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+	public static void main(String[] args) {
+
+		EmployeeWageComputation call = new EmployeeWageComputation();
+
+		call.printMessage();
+	}
+}

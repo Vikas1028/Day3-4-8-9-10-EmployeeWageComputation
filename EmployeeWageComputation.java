99ba10e999ce0eb2f2b4ac23cac235472c0e85a5
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-30 00:01:46 +0530

    Add: Employee Attendance

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,10 +1,25 @@
 package EmployeeWageComputation;
 
+import java.util.Random;
+
 public class EmployeeWageComputation {
+
+	public int Presenti;
 
 	// Start with displaying welcome message
 	public void printMessage() {
 		System.out.println("Welcome to Employee Wage Computation Program");
+	}
+
+	// Check employee is present or absent
+	public void checkEmployeeAttendance() {
+		Random num = new Random();
+		Presenti = num.nextInt(2);
+		if (Presenti == 0) {
+			System.out.println("Employee is Absent");
+		} else {
+			System.out.println("Employee is Present");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -12,5 +27,7 @@ public class EmployeeWageComputation {
 		EmployeeWageComputation call = new EmployeeWageComputation();
 
 		call.printMessage();
+
+		call.checkEmployeeAttendance();
 	}
 }

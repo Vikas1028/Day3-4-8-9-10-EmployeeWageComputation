2eca07b4fbf522e0804c0d25877b07e7ab2a1c7c
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-09-30 14:32:54 +0530

    Add: Calculate Monthly Employee Wage

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -7,7 +7,8 @@ public class EmployeeWageComputation {
 	public int Presenti, Presenti2;
 	public int WagePerHour, FullDayHours, HalfDayHours, Hours;
 	public int PartTimeWagePerHour;
-	public int AddEmployee2;
+	public int AddEmployee2, Month, WageForMonth = 0, Month2, WageForMonth2 = 0;
+	public int FullDay = 0, HalfDay = 0, FullDay2 = 0, HalfDay2 = 0;
 
 	// Start with displaying welcome message
 	public void printMessage() {
@@ -44,6 +45,26 @@ public class EmployeeWageComputation {
 
 	}
 
+	// Calculate FullTime Employee Wage for Month
+	public void WageForMonth() {
+		for (int i = 1; i <= 20; i++) {
+			Random num4 = new Random();
+			Month = num4.nextInt(2);
+			switch (Month) {
+			case 0:
+				WageForMonth = WageForMonth + 20 * 8;
+				FullDay = FullDay + 1;
+				break;
+			case 1:
+				WageForMonth = WageForMonth + 20 * 4;
+				HalfDay = HalfDay + 1;
+				break;
+			}
+		}
+		System.out.println("This Month Employee work FullTime " + FullDay + " Days and HalfTime " + HalfDay + " Days.");
+		System.out.println("This Month Full Time Employee Wage is " + WageForMonth + " Rs.");
+	}
+
 	// Add part time Employee and Wage
 	public void AddPartTimeEmployee() {
 		System.out.println("Part Time Employee Added");
@@ -69,6 +90,27 @@ public class EmployeeWageComputation {
 			}
 		}
 
+	}
+
+	// Calculate PartTime Employee Wage for Month
+	public void WageForMonth2() {
+		for (int i = 0; i <= 20; i++) {
+			Random num4 = new Random();
+			Month2 = num4.nextInt(2);
+			switch (Month2) {
+			case 0:
+				WageForMonth2 = WageForMonth2 + 10 * 8;
+				FullDay2 = FullDay2 + 1;
+				break;
+			case 1:
+				WageForMonth2 = WageForMonth2 + 10 * 4;
+				HalfDay2 = HalfDay2 + 1;
+				break;
+			}
+		}
+		System.out
+				.println("This Month Employee work FullTime " + FullDay2 + " Days and HalfTime " + HalfDay2 + " Days.");
+		System.out.println("This Month Part Time Employee Wage is " + WageForMonth2 + " Rs.");
 	}
 
 	// Solve Using Switch Case
@@ -109,7 +151,11 @@ public class EmployeeWageComputation {
 
 		call.calculateEmployeeDailyWage();
 
+		call.WageForMonth();
+
 		call.AddPartTimeEmployee();
+
+		call.WageForMonth2();
 
 		call.solveUsingSwitchCase();
 	}

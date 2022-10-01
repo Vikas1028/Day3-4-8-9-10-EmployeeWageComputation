a9f7364b7785f03dec4394217d12cf61de9494f3
Vikas1028 <vikasbhandekar2018@gmail.com>
2022-10-01 11:04:35 +0530

    calculate wage till conditon of total working hours and days is reached for a Month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -9,6 +9,8 @@ public class EmployeeWageComputation {
 	public int PartTimeWagePerHour;
 	public int AddEmployee2, Month, WageForMonth = 0, Month2, WageForMonth2 = 0;
 	public int FullDay = 0, HalfDay = 0, FullDay2 = 0, HalfDay2 = 0;
+	public int conditionWage = 0, conditionWage2;
+	public int partTimeWagePerHour;
 
 	// Start with displaying welcome message
 	public void printMessage() {
@@ -52,17 +54,38 @@ public class EmployeeWageComputation {
 			Month = num4.nextInt(2);
 			switch (Month) {
 			case 0:
-				WageForMonth = WageForMonth + 20 * 8;
+				WageForMonth = WageForMonth + WagePerHour * FullDayHours;
 				FullDay = FullDay + 1;
 				break;
 			case 1:
-				WageForMonth = WageForMonth + 20 * 4;
+				WageForMonth = WageForMonth + WagePerHour * HalfDayHours;
 				HalfDay = HalfDay + 1;
 				break;
 			}
 		}
 		System.out.println("This Month Employee work FullTime " + FullDay + " Days and HalfTime " + HalfDay + " Days.");
 		System.out.println("This Month Full Time Employee Wage is " + WageForMonth + " Rs.");
+	}
+
+	// Calculate FullTime Employee2 Wage for Month till condition match 100 hours or
+	// 20 days.
+	public void WageForMonthWithCondition() {
+		int hours = 00, days = 0, num;
+		Random inp = new Random();
+		while (hours <= 100 && days <= 20) {
+			num = inp.nextInt(2);
+			if (num == 0) {
+				hours = hours + 4;
+				conditionWage = conditionWage + WagePerHour * HalfDayHours;
+			} else {
+				hours = hours + 8;
+				conditionWage = conditionWage + WagePerHour * FullDayHours;
+			}
+			days = days + 1;
+		}
+		System.out.println("Wage per Month of Full Time Employee till condition matches 100hr. or 20 days work is "
+				+ conditionWage + " Rs.");
+
 	}
 
 	// Add part time Employee and Wage
@@ -94,16 +117,17 @@ public class EmployeeWageComputation {
 
 	// Calculate PartTime Employee Wage for Month
 	public void WageForMonth2() {
+		partTimeWagePerHour = 10;
 		for (int i = 0; i <= 20; i++) {
 			Random num4 = new Random();
 			Month2 = num4.nextInt(2);
 			switch (Month2) {
 			case 0:
-				WageForMonth2 = WageForMonth2 + 10 * 8;
+				WageForMonth2 = WageForMonth2 + partTimeWagePerHour * partTimeWagePerHour * FullDayHours;
 				FullDay2 = FullDay2 + 1;
 				break;
 			case 1:
-				WageForMonth2 = WageForMonth2 + 10 * 4;
+				WageForMonth2 = WageForMonth2 + partTimeWagePerHour * HalfDayHours;
 				HalfDay2 = HalfDay2 + 1;
 				break;
 			}
@@ -111,6 +135,27 @@ public class EmployeeWageComputation {
 		System.out
 				.println("This Month Employee work FullTime " + FullDay2 + " Days and HalfTime " + HalfDay2 + " Days.");
 		System.out.println("This Month Part Time Employee Wage is " + WageForMonth2 + " Rs.");
+	}
+
+	// Calculate FullTime Employee2 Wage for Month till condition match 100 hours or
+	// 20 days.
+	public void WageForMonthWithCondition2() {
+		int hours = 00, days = 0, num;
+		Random inp = new Random();
+		while (hours <= 100 && days <= 20) {
+			num = inp.nextInt(2);
+			if (num == 0) {
+				hours = hours + 4;
+				conditionWage2 = conditionWage2 + partTimeWagePerHour * HalfDayHours;
+			} else {
+				hours = hours + 8;
+				conditionWage2 = conditionWage2 + partTimeWagePerHour * FullDayHours;
+			}
+			days = days + 1;
+		}
+		System.out.println("Wage per Month of Part Time Employee till condition matches 100hr. or 20 days work is "
+				+ conditionWage2 + " Rs.");
+
 	}
 
 	// Solve Using Switch Case
@@ -153,9 +198,13 @@ public class EmployeeWageComputation {
 
 		call.WageForMonth();
 
+		call.WageForMonthWithCondition();
+
 		call.AddPartTimeEmployee();
 
 		call.WageForMonth2();
+		
+		call.WageForMonthWithCondition2();
 
 		call.solveUsingSwitchCase();
 	}
